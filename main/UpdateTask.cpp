98823c222eb86e9d3ec424875dda7c48544c5962
andy-linux <andreas.loehr97@gmail.com>
2026-03-27 08:47:59 +0100

    udpate

--- main/UpdateTask.cpp
+++ main/UpdateTask.cpp
@@ -2,16 +2,93 @@
 #include "freertos/task.h"
 
 #include "esp_http_client.h"
+#include "esp_log.h"
+#include <fmt/format.h>
+#include <iostream>
 #include <string>
 
+static const char *TAG = "HTTP_CLIENT";
+
+SensorData::SensorData(const std::string &device_id,
+                       const std::string &sensor_type, double sensor_value,
+                       const std::string &unit, const std::string &timestamp)
+    : device_id(device_id), sensor_type(sensor_type),
+      sensor_value(sensor_value), unit(unit), timestamp(timestamp) {}
+
+std::string SensorData::to_string() const {
+  return fmt::format(
+      R"({{"device_id":"{}","sensor_type":"{}","sensor_value":{},"unit":"{}","timestamp":"{}"}})",
+      device_id, sensor_type, sensor_value, unit, timestamp);
+}
+
 void UpdateTask::task() {
+  /*
+   * Need client config
+   * event handler
+   */
 
   const char *url_to_push_to = "http://localhost";
-  int port = 8080;
-  const char *host = "localhost";
+  // int port = 8080;
+  // const char *host = "localhost";
+  constexpr uint MAX_HTTP_OUTPUT_BUFFER = 2000;
 
-  esp_http_client_config_t config = {.url = url_to_push_to, .port = port};
+  // fake data
+  SensorData data("device123", "temperature", 255.0, "Celsius",
+                  "2024-06-27T12:00:00Z");
+
+  std::cout << data.to_string() << std::endl;
+  // esp_http_client_config_t config = {.url = url_to_push_to, .port = port};
+  // esp_http_client_handle_t client = esp_http_client_init(&config);
+
+  // **** THIS IS THE FIELDS EXPECTED ****
+
+  // // Extract data from JSON
+  // std::string device_id = json["device_id"].s();
+  // std::string sensor_type = json["sensor_type"].s();
+  // double sensor_value = json["sensor_value"].d();
+  // std::string unit = json.has("unit") ? std::string(json["unit"].s()) :
+  // std::string(""); std::string timestamp = json.has("timestamp") ?
+  // std::string(json["timestamp"].s()) : std::string("");
+
+  // Declare local_response_buffer with size (MAX_HTTP_OUTPUT_BUFFER + 1) to
+  // prevent out of bound access when it is used by functions like strlen(). The
+  // buffer should only be used upto size MAX_HTTP_OUTPUT_BUFFER
+  char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
+  /**
+   * NOTE: All the configuration parameters for http_client must be specified
+   * either in URL or as host and path parameters. If host and path parameters
+   * are not set, query parameter will be ignored. In such cases, query
+   * parameter should be specified in URL.
+   *
+   * If URL as well as host and path parameters are specified, values of host
+   * and path will be considered.
+   */
+  esp_http_client_config_t config = {
+      .url = url_to_push_to,
+      .host = url_to_push_to,
+      .port = 8080,
+      .path = "/api/data",
+      .user_data =
+          local_response_buffer, // Pass address of local buffer to get response
+                                 //.disable_auto_redirect = true,
+  };
   esp_http_client_handle_t client = esp_http_client_init(&config);
+
+  // POST
+  const char *post_data = data.to_string().c_str();
+  esp_http_client_set_url(client, "http://localhost:8080/api/data");
+  esp_err_t err;
+  esp_http_client_set_method(client, HTTP_METHOD_POST);
+  esp_http_client_set_header(client, "Content-Type", "application/json");
+  esp_http_client_set_post_field(client, post_data, strlen(post_data));
+  err = esp_http_client_perform(client);
+  if (err == ESP_OK) {
+    ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %" PRId64,
+             esp_http_client_get_status_code(client),
+             esp_http_client_get_content_length(client));
+  } else {
+    ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+  }
 
   while (true) {
     printf("Update Task is running ...\n");
--- main/include/UpdateTask.h
+++ main/include/UpdateTask.h
@@ -2,8 +2,26 @@
 #include "freertos/task.h"
 #include "portmacro.h"
 #include <clocale>
+#include <format>
+#include <string>
 
 #include "esp_http_client.h"
+
+class SensorData {
+private:
+  std::string device_id;
+  std::string sensor_type;
+  double sensor_value;
+  std::string unit;
+  std::string timestamp;
+
+public:
+  SensorData(const std::string &device_id, const std::string &sensor_type,
+             double sensor_value, const std::string &unit,
+             const std::string &timestamp);
+
+  std::string to_string() const;
+};
 
 class UpdateTask {
 private:
